7a47e9a2748ad89c21e8cb0b5c6a504f2aa3e899
James Shepherd <jamesshep11@gmail.com>
2023-04-23 19:08:11 +0200

    Code clean up
    Upadted PSR to make code easier to read. Added comments and doc blocks to make code easier to understand

--- Assignment1/src/com/example/neuron/Main.java
+++ Assignment1/src/com/example/neuron/Main.java
@@ -15,10 +15,12 @@ public class Main {
         BufferedReader console = new BufferedReader(
                 new InputStreamReader(System.in));
 
+        // Get training data
         System.out.println("Enter training file:");
         String fileName = console.readLine();
         readFile(fileName);
 
+        // Train neuron using gradient decent method
         Neuron neuron = new Neuron();
         neuron.gradientDescent(patterns, results);
 
@@ -27,10 +29,12 @@ public class Main {
         patterns.clear();
         results.clear();
 
+        // Get test data
         System.out.println("Enter test file:");
         fileName = console.readLine();
         readFile(fileName);
 
+        // Use neuron to process test data and evaluate accuracy of neuron after training
         double SSE = 0;
         for (int i = 0; i < patterns.size(); i++) {
             int mark = neuron.run(patterns.get(i));
@@ -42,34 +46,50 @@ public class Main {
         patterns.clear();
         results.clear();
 
+        // Get data to be evaluated
         System.out.println("Enter evaluation file:");
         fileName = console.readLine();
         readFile(fileName);
 
+        // Evaluate input data with trained neuron
         for (int i = 0; i < patterns.size(); i++) {
             int mark = neuron.run(patterns.get(i));
             System.out.println(mark);
         }
     }
 
+    /**
+     * Reads data from file and extracts data into input arrays and answers (if training data)
+     *
+     * @param fileName Name of input file
+     */
     public static void readFile(String fileName) {
         try {
             File file = new File(fileName);
             Scanner reader = new Scanner(file);
 
             int patternCount = 0;
+
             while (reader.hasNextLine()) {
+                // Read line from file
                 String line = reader.nextLine();
                 String[] patternString = line.split(",");
                 ArrayList<Integer> pattern = new ArrayList<>();
+
+                // Extract line into array of int
                 for (int i = 0; i < 3; i++)
                     pattern.add(Integer.parseInt(patternString[i]));
+
+                // Add array to list of input data
                 patterns.put(patternCount++, pattern);
+
+                // Add 4th element as correct answer (if training data)
                 if (patternString.length == 4)
                     try {
                         results.add(Integer.parseInt(patternString[3]));
                     } catch (Exception ignored){ }
             }
+
             reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error reading from file.");
--- Assignment1/src/com/example/neuron/Neuron.java
+++ Assignment1/src/com/example/neuron/Neuron.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * A single neuron which takes in 3 int values and outputs 1 int value
+ */
 public class Neuron {
+    // The weighting given to each input value ()to be tunes during training
     private ArrayList<Double> weights;
 
     public Neuron() {
     }
 
+    /**
+     * Processes data after the neuron has been trained (does not learn/improve)
+     *
+     * @param pattern Array of 3 int values as input to the neuron
+     * @return Int value after evaluating input
+     */
     public int run(ArrayList<Integer> pattern){
         int sum = 0;
+
         for (int i = 0; i < pattern.size(); i++)
             sum += weights.get(i) * pattern.get(i);
+
         sum += weights.get(weights.size()-1);
 
         //System.out.println(sum);
@@ -21,43 +33,63 @@ public class Neuron {
         return sum;
     }
 
+    /**
+     * Gradient descent training algorithm initializes weights to random (small) values and then repeatedly adjust
+     * weights according to: weight - (n * (-2 * (expectedResult - actualResult) * (inputValue)))
+     *
+     * @param patterns List of input patterns
+     * @param results List of expected output values
+     */
     public void gradientDescent(HashMap<Integer, ArrayList<Integer>> patterns, ArrayList<Integer> results){
-        // Initialise weights to small random values
         Random random = new Random();
         weights = new ArrayList<>();
+
+        // Initialise weights to small random values
         for (int i = 0; i < patterns.get(0).size(); i++)
             weights.add(random.nextDouble() * (Math.pow(-1,random.nextInt(2))));
-        weights.add(random.nextDouble() * (Math.pow(-1, random.nextInt(2))));
+
+        weights.add(random.nextDouble() * (Math.pow(-1, random.nextInt(2)))); // bias weight
 
         for (double w : weights)
             System.out.print(w + ", ");
+
         System.out.println();
 
-        // Train
-        double SSE = Double.MAX_VALUE;
+        // Start training
+        double SSE = Double.MAX_VALUE; // performance measure (Sum of Squared Errors)
         int count = 0;
-        double n = 0.0001;
+        double n = 0.0001; // learning rate
+
+        // While error is too high
         while (SSE > 250) {
             SSE = 0;
+
             // for each pattern
             for (int p = 0; p < weights.size(); p++) {
                 ArrayList<Integer> pattern = patterns.get(p);
                 int expected = results.get(p);
                 //int diff = expected - run(pattern);
+
                 // adjust each weight (except bias)
                 int i;
                 for (i = 0; i < weights.size()-1; i++)
                     weights.set(i, weights.get(i) - (n * (-2 * (expected - run(pattern)) * pattern.get(i))));
+
                 // adjust bias weight
                 weights.set(i, weights.get(i) - (n * (-2 * (expected - run(pattern)))));
 
                 // Calculate Sum Squared Error
                 SSE += Math.pow(expected - run(pattern),2);
             }
+
             count++;
             System.out.println(count + " iterations. SSE = " + SSE);
         }
+
+        // Output final performance
         System.out.println("SSE = " + SSE);
+
+        // Output final (trained) weights
         for (double w : weights)
             System.out.print(w + ", ");
     }
